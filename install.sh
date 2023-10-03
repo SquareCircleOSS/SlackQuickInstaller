fff6e39b7abc7f4d2d1a56ced07485c7e9e9b301
j33h22 <ju33huang22@gmail.com>
2023-10-03 22:52:12 +0800

    Make the install script can add directory into PATH

--- install.sh
+++ install.sh
@@ -14,11 +14,15 @@ cd /SlackQuickInstaller
 echo "Copy the files of SlackQuickInstaller"
 cp -r ./* /SlackQuickInstaller/
 
+echo "Adding the directory into PATH"
+echo "PATH=$PATH:/SlackQuickInstaller" > ~/.bashrc
+echo "PATH=$PATH:/SlackQuickInstaller" > /etc/environment
+
 echo "Cloning the git repo from SlackBuilds..."
 git clone git://git.slackbuilds.org/slackbuilds ./Slackbuilds
 
 echo "Congratulations! Now you can use SlackQuickInstaller to install any packages from SlackBuilds."
 echo "############"
 echo "Use it:"
-echo "sudo /SlackQuickInstaller/sqi.sh package_name"
+echo "sudo sqi.sh package_name"
 echo "############"
